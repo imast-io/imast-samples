e334077a09d263991aac23b0e049abfa04d2262f
Davit Petrosyan <davit.petrosyan.sc@gmail.com>
2021-03-11 22:48:04 +0400

    Removed scheduler sample. Moved to Work4j repository

--- scheduler/mysqlcluster/init/1.0.mysql.quartz.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE quartz_scheduler;
-
-USE quartz_scheduler;
-
-CREATE USER 'workeruser'@'%' IDENTIFIED BY 'workerpassword';
-GRANT ALL PRIVILEGES ON quartz_scheduler.* TO 'workeruser'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-
-
